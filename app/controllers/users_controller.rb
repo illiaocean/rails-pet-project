8193e6386afa40604d018ac326430a902a749c5a
Illia Abdullaiev <illia.abdullaiev@gmail.com>
2020-01-05 11:38:09 -0800

    add user avatars

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,7 @@ class UsersController < ApplicationController
 
   def update
     respond_to do |format|
+      @user.avatar.attach(user_params[:avatar])
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
