75b3d99af8bcd7d403ee4dd76d169a418ed4718e
Illia Abdullaiev <illia.abdullaiev@gmail.com>
2020-01-12 16:46:10 -0800

    add follow relationship between users

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,12 @@ class User < ApplicationRecord
   validates :bio, length: { maximum: 1000 }
 
   has_secure_password
+
+  def follow(user_id)
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
+  end
 end
